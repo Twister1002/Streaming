231ec572690b2d3919b45a2ac1c5fbc64d7f93e3
Twister <twister1002@gmail.com>
2017-12-30 06:50:40 -0500

    Form now saves user input to settings file.

--- library/js/settings.js
+++ library/js/settings.js
@@ -1,34 +1,7 @@
-const {app,ipcRenderer} = require("electron");
-const fs = require("fs");
-const path = require("path");
+const settings = require("../library/modules/settings.js");
+const submitButton = document.getElementById("update_settings_button");
 
-class Settings {
-
-    constructor() {
-        this._file = "";
-        this._data = {};
-        this.Init();
-    }
-
-    Init() {
-        if (this._file == "") {
-            this._file = path.join(ipcRenderer.sendSync("sync:app", "userData"), "settings.json");
-        }
-
-        // Verify that the settings file exists. If not, create it.
-        if (!fs.existsSync(this._file)) {
-            // Load default settings
-            var defaultSettings = JSON.parse(fs.readFileSync("settings-default.json", "utf-8"));
-            fs.writeFileSync(path.join(ipcRenderer.sendSync("sync:app", "userData"), "settings.json"), JSON.stringify(defaultSettings));
-        }
-
-        this._data = JSON.parse(fs.readFileSync(this._file, "utf-8"));
-    }
-
-    get data() {
-        return this._data;
-    }
-
-}
-
-module.exports = new Settings();
+submitButton.addEventListener("click", (e) => {
+    var fields = document.querySelectorAll(".field input");
+    settings.Update(fields);
+});
