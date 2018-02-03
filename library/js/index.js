ca4c9d376f68b17fd6d1b6a9546a403071c37af4
Twister <twister1002@gmail.com>
2018-02-03 02:14:04 -0500

    Loaded settings from local file. Added settings page stylesheet.

--- library/js/index.js
+++ library/js/index.js
@@ -3,14 +3,42 @@ const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
 
-
 const util = require("../library/modules/utilities.js");
 const settings = require("../library/modules/settings.js");
 const twitch = require("../library/modules/twitch.js");
 
-// Load the twitch data
-(async () => {
-    await twitch.GetUser(settings.GetFieldValue("twitch", "name"));
-    twitch.GetRecentFollower();
-    twitch.GetRecentSubscriber();
+// Load the Information saved from the user
+(function() {
+    let titleSettings = settings.GetType("title");
+    let layoutSettings = settings.GetType("layout");
+
+    for (let setting in titleSettings) {
+        let settingField = document.querySelector(".title_"+setting)
+
+        if (settingField) {
+            if (setting.includes("_file")) {
+                let fileData = fs.readFile(titleSettings[setting], "utf-8", (err, data) => {
+                    settingField.textContent = data;
+                });
+            }
+            else {
+                settingField.textContent = titleSettings[setting];
+            }
+        }
+    }
+
+    for (let setting in layoutSettings) {
+        switch (setting) {
+            case "follow_border-color":
+                let borderColor = document.querySelectorAll("dt, dd");
+                for (let settingField of borderColor) {
+                    settingField.style["border-color"] = layoutSettings[setting];
+                }
+                break;
+            default:
+                let settingField = document.querySelector(".layout_"+setting)
+                document.querySelector("body").style[setting] = layoutSettings[setting];
+                break;
+        }
+    }
 })();
