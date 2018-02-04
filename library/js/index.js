e758761b1c2ba817f6afaad9e87b8150e7a8c255
Twister <twister1002@gmail.com>
2018-02-04 16:27:40 -0500

    Updated the social icons to transition in and out.

--- library/js/index.js
+++ library/js/index.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const util = require("../library/modules/utilities.js");
 const settings = require("../library/modules/settings.js");
 const twitch = require("../library/modules/twitch.js");
+const social = require("../library/modules/social.js");
 const socialElement = document.querySelector(".social ul");
 
 // Load the Information saved from the user
@@ -54,18 +55,6 @@ const socialElement = document.querySelector(".social ul");
         socialElement.appendChild(element);
     }
     
-    SocialDisplay();
+    social.SetElements(socialElement);
+    social.Start();
 })();
-
-function SocialDisplay() {
-    setInterval(function() {
-        let element = socialElement.children[0];
-        if (element.classList.contains("display")) {
-            element.classList.remove("display");
-        }
-        else {
-            element.classList.add("display");
-        }
-    }, 2000);
-    
-}
